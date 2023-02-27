cf183a212f575ca302a9626cf214ba2bd3158a64
Jiri Kalous <kalous.jiri21@gmail.com>
2023-02-27 12:01:23 +0100

    portfolio added

---
+++ src/app/portfolio.ts
@@ -0,0 +1,56 @@
+export const items = [
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    {
+     name: 'Photo X',
+     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+     img: '../assets/union-jack.png',
+     longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
+    },
+    ]
