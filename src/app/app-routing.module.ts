b6d94b9a8ace15f056d03988d51fce61850e7571
king-of-chardonnay <tanervk@gmail.com>
2023-02-27 11:21:56 +0100

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CustomersComponent } from './customers/customers.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: LandingPageComponent
+},{
+  path: "about", component: AboutComponent
+},{
+  path: "customers", component: CustomersComponent
+}, {
+  path: "contact", component: ContactComponent
+}, {
+  path: "**", redirectTo: ""
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
