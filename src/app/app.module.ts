7d259472f3b7efe434e8069a24d46cd6537aba8f
Jiri Kalous <kalous.jiri21@gmail.com>
2023-02-27 10:48:09 +0100

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AwardsComponent } from './awards/awards.component';
+import { ContactComponent } from './contact/contact.component';
+import { CustomersComponent } from './customers/customers.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MediaComponent } from './media/media.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AwardsComponent,
+    ContactComponent,
+    CustomersComponent,
+    LandingPageComponent,
+    MediaComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
