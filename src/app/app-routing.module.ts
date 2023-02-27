3b4fd3df896bb835e76aa116d257057e4091312e
Jiri Kalous <kalous.jiri21@gmail.com>
2023-02-27 14:29:55 +0100

    portfolio path added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CustomersComponent } from './customers/customers.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
 
 const routes: Routes = [{
   path: "", component: LandingPageComponent
@@ -15,6 +16,8 @@ const routes: Routes = [{
   path: "contact", component: ContactComponent
 }, {
   path: "**", redirectTo: ""
+},{
+  path: 'portfolio/:portfolioId', component: PortfolioDetailsComponent
 }
 ];
 
--- src/app/portfolio-details/portfolio-details.component.ts
+++ src/app/portfolio-details/portfolio-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { portfolio } from '../portfolio';
+import { IPortfolio } from '../IPortfolio';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio-details',
   templateUrl: './portfolio-details.component.html',
   styleUrls: ['./portfolio-details.component.css']
 })
-export class PortfolioDetailsComponent {
+export class PortfolioDetailsComponent implements OnInit {
+  portfolio: IPortfolio = {} as IPortfolio;
+  id: number = 0;
+
+  constructor(
+    private route: ActivatedRoute
+      ) {}
+
+      ngOnInit(): void {
+        this.route.params.subscribe((params: Params) => {
+          this.id = +params['portfolioId'];
+          this.portfolio = portfolio[this.id];
+        });
+      }
 
 }
