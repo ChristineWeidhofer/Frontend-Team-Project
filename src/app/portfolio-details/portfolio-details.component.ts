bc6caebefc070697ee00d6ca82dd6676eb0d21f8
Jiri Kalous <kalous.jiri21@gmail.com>
2023-02-27 14:00:08 +0100

    portfolio details created

---
+++ src/app/portfolio-details/portfolio-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-details',
+  templateUrl: './portfolio-details.component.html',
+  styleUrls: ['./portfolio-details.component.css']
+})
+export class PortfolioDetailsComponent {
+
+}
