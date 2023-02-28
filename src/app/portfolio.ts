827cb41ed86b08737a636e1d057612e040bc125e
lamiaap <lamiaap2001@gmail.com>
2023-02-28 10:35:51 +0100

    push img

--- src/app/portfolio.ts
+++ src/app/portfolio.ts
@@ -44,13 +44,13 @@ export const portfolio = [
     {
      name: 'Photo X',
      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
-     img: '../assets/union-jack.png',
+     img: '../assets/graduate.jpg',
      longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
     },
     {
      name: 'Photo X',
      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
-     img: '../assets/union-jack.png',
+     img: '../assets/travel.jpg',
      longDes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos.',
     },
     ]
