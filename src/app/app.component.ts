7fbf76a757cc71ef6a0e475ccaf42e9f6d49dc3a
ChristineWeidhofer <xtine@gmx.at>
2023-03-04 16:39:40 +0100

    add scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Team10';
+
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
+
+  windowScrolled = false;
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset !== 0;
+    });
+  }
 }
